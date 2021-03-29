7557968f1eb1e83274d935396a4b63c7a7ac9ece
ilyBreathless <72036113+ilyBreathless@users.noreply.github.com>
2021-03-29 18:50:24 +0300

    fix: remove bug

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,10 +1,7 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
     maxDepth = 0
     currDepth = 0
-    calculateDepth(arr) {
+        calculateDepth(arr) {
         this.currDepth++
         if (arr.length > 0) {
             for (let i = 0; i < arr.length; i++)
